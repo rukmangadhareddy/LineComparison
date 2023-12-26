791777a19750a02e0f7aeb04342205c59da1e62e
rukmangadhareddy <rukmangadhareddy43@gmail.com>
2023-12-26 13:57:43 +0530

    UC3LinePoints

---
+++ src/com/bridgelabzs/LinePoints.java
@@ -0,0 +1,27 @@
+package com.bridgelabzs;
+
+public class LinePoints {
+
+    /**
+     * taking x and y instance variables
+     */
+    private double x;
+    private double y;
+    /**
+     *providing set and get method
+     * assign value use set method
+     * get value use get method
+     */
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+}
